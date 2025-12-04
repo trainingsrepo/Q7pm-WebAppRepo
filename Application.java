be038a9a28f1a5a9e6307f7699f2369f07534af3
trainingsrepo <trainings.anil@gmail.com>
2025-12-04 19:29:03 +0530

    feature101 changes in Application.java file by vivek

--- Application.java
+++ Application.java
@@ -18,4 +18,18 @@ public class ProgramStructure {
     public static void greet() {
         System.out.println("Greetings from the method world!");
     }
+	#code changes for feature101 by Vivek -Oct 2024 of UserTransaction Module
+	  public void getCheckingDepositInput(){
+        System.out.println("Checking Account Balance: " + moneyFormat.format(checkingBalance));
+        System.out.print("Amount to deposit to Checking Account: ");
+        double amount = input.nextDouble();
+
+        if (amount <= 0) {
+            System.out.println("Invalid amount. Please enter a positive number.");
+        } else {
+            calcCheckingDeposit(amount);
+            System.out.println("New Checking Account Balance: " + moneyFormat.format(checkingBalance));
+        }
+    }
+	#end of feature101 code
 }
